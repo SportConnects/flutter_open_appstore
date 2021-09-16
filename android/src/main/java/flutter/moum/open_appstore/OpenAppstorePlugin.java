23cc370292e068ad27b01667c37205c3e90d80db
Sascha Heim <developers@think-big-company.com>
2021-09-17 00:21:34 +0200

    Feature/android v2

--- android/src/main/java/flutter/moum/open_appstore/OpenAppstorePlugin.java
+++ android/src/main/java/flutter/moum/open_appstore/OpenAppstorePlugin.java
@@ -12,59 +12,51 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** OpenAppstorePlugin */
-public class OpenAppstorePlugin implements FlutterPlugin {
-  /** Plugin registration. */
-
+public class OpenAppstorePlugin implements MethodCallHandler, FlutterPlugin {
+  
+  private Context applicationContext;
   private MethodChannel channel;
+  private static final String CHANNEL_NAME = "moum/open_appstore";
 
-  public static void registerWith(Registrar registrar) {
-
-    OpenAppstorePlugin plugin = new OpenAppstorePlugin();
-    plugin.setMethodChannel(registrar.context(), registrar.messenger());
-  }
-
-  private void setMethodChannel(final Context context, BinaryMessenger messenger) {
-
-    channel = new MethodChannel(messenger, "flutter.moum.open_appstore");
-    channel.setMethodCallHandler(new MethodCallHandler() {
-      @Override
-      public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        if (call.method.equals("getPlatformVersion")) {
-          result.success("Android " + android.os.Build.VERSION.RELEASE);
-        }
-        else if (call.method.equals("openappstore")) {
-          result.success("Android " + android.os.Build.VERSION.RELEASE);
-          String android_id = call.argument("android_id");
-          String manufacturer = android.os.Build.MANUFACTURER;
-          if (manufacturer.equals("Amazon")) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("amzn://apps/android?p=" + android_id)));
-          } else {
-            try {
-              context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + android_id)));
-            } catch (android.content.ActivityNotFoundException e) {
-              context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + android_id)));
-            }
-          }
-          result.success(null);
-        }
-        else {
-          result.notImplemented();
-        }
-      }
-    });
-  }
-
+  /**
+   * Plugin registration.
+   */
 
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-    setMethodChannel(binding.getApplicationContext(), binding.getBinaryMessenger());
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
+    channel.setMethodCallHandler(this);
+    applicationContext = flutterPluginBinding.getApplicationContext();
   }
 
   @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+  public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
     channel.setMethodCallHandler(null);
+    applicationContext = null;
+  }
+  
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else if (call.method.equals("openappstore")) {
+		result.success("Android " + android.os.Build.VERSION.RELEASE);
+		String android_id = call.argument("android_id");
+		String manufacturer = android.os.Build.MANUFACTURER;
+		if (manufacturer.equals("Amazon")) {
+			context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("amzn://apps/android?p=" + android_id)));
+		} else {
+			try {
+				context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + android_id)));
+			} catch (android.content.ActivityNotFoundException e) {
+				context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + android_id)));
+            }
+		}
+		result.success(null);
+	} else {
+		result.notImplemented();
+    }
   }
 }
